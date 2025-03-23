20da9708ca51a2695bc350ef56e42afa046587a8
Thimira Navodana <thimiranavodana2002@gmail.com>
2025-03-23 19:26:36 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -38,7 +38,7 @@ function App() {
         <Router>
           <Navbar />
           <Routes>
-            <Route path="/Kasun-s/" element={<Home />} />
+            <Route path="/" element={<Home />} />
             <Route path="/home" element={<Home />} />
             <Route path="/about" element={<About />} />
             <Route path="/products" element={<Products />} />
